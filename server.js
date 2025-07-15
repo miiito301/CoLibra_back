bfbdec90989327097ffe2b40c03a9c3b0a12e581
miiito301 <ikaruga0131@gmail.com>
2025-07-15 19:43:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.use('/api/reviews', reviewRoutes)
 
 // DB接続
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => {
   console.log("✅ MongoDB connected")
