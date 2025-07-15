1373e6dfa17bfd660743387a81ea73d3d25f998b
miiito301 <ikaruga0131@gmail.com>
2025-07-15 17:57:54 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+//server.js
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import reviewRoutes from './routes/reviews.js'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// ルーティング
+app.use('/api/reviews', reviewRoutes)
+
+// DB接続
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("✅ MongoDB connected")
+  app.listen(process.env.PORT, () =>
+    console.log(`🚀 Server running on port ${process.env.PORT}`)
+  )
+})
+.catch((err) => console.error("DB connection error:", err))
